1aaaa345f1c4aa8ef157db8719f5bd46ba48b778
trudie anderson <trudiekennedy@gmail.com>
2025-04-24 13:10:24 +0100

    Updates to seed data

--- seeds/recipes.sql
+++ seeds/recipes.sql
@@ -1,4 +1,4 @@
-x-- The job of this file is to reset all of our important database tables.
+-- The job of this file is to reset all of our important database tables.
 -- And add any data that is needed for the tests to run.
 -- This is so that our tests, and application, are always operating from a fresh
 -- database state, and that tests don't interfere with each other.
@@ -8,17 +8,17 @@ DROP TABLE IF EXISTS recipes;
 DROP SEQUENCE IF EXISTS recipes_id_seq;
 
 -- Then, we recreate them
-CREATE SEQUENCE IF NOT EXISTS _id_seq;
+CREATE SEQUENCE IF NOT EXISTS recipes_id_seq;
 CREATE TABLE recipes (
   id SERIAL PRIMARY KEY,
   name TEXT,
-  cooking_time INTERVAL,
+  cooking_time int,
   rating int
 );;
 
 -- Finally, we add any records that are needed for the tests to run
-INSERT INTO recipes (name, cooking_time, rating) VALUES ('Salmon in a Soy and Ginger Sauce', '35 minutes', 5);
-INSERT INTO recipes (name, cooking_time, rating) VALUES ('Tuna & Rice', '1 hour 15 minutes', 4);
-INSERT INTO recipes (name, cooking_time, rating) VALUES ('Mince & Tatties', '1 hour', 2);
-INSERT INTO recipes (name, cooking_time, rating) VALUES ('Chicken with Mushroom and White Wine Sauce', '45 minutes', 3);
-INSERT INTO recipes (name, cooking_time, rating) VALUES ('Chicken Fajitas', '30 minutes', 4);
+INSERT INTO recipes (name, cooking_time, rating) VALUES ('Salmon in a Soy and Ginger Sauce', 35, 5);
+INSERT INTO recipes (name, cooking_time, rating) VALUES ('Tuna & Rice', 75, 4);
+INSERT INTO recipes (name, cooking_time, rating) VALUES ('Mince & Tatties', 60, 2);
+INSERT INTO recipes (name, cooking_time, rating) VALUES ('Chicken with Mushroom and White Wine Sauce', 45, 3);
+INSERT INTO recipes (name, cooking_time, rating) VALUES ('Chicken Fajitas', 30, 4);
